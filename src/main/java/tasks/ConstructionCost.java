a35fdf58b5c18f0790116c4cf0fecc3e0f4c3dea
lokesh942018 <lokehdammalapati1999@gmail.com>
2020-02-25 23:26:21 +0400

    Task Done

--- src/main/java/tasks/ConstructionCost.java
+++ src/main/java/tasks/ConstructionCost.java
@@ -9,37 +9,39 @@ class ConstructionCost
 	{
 			double constructionCost;
 		    Scanner sc=new Scanner (System. in);
-		    LOGGER.fatal("Choose the Material Standard of the House:");
-		    LOGGER.fatal("Standard materials-->0\nAbove Standard materials-->1\nHigh Standard materials-->2");		   
-		    LOGGER.fatal("High Standard and Auotomated house-->3");
+		    LOGGER.fatal("Choose the Material Standard of the House:\n");
+		    LOGGER.fatal("Standard materials-->0\n");
+		    LOGGER.fatal("Above Standard materials-->1\n");
+		    LOGGER.fatal("High Standard Materials-->2\n");
+		    LOGGER.fatal("High Standard and Auotomated house-->3\n");
 		    int material=sc.nextInt();
 		    if(material>=4 ) {
 		    	LOGGER.fatal("Please choose Material Standard as Mentioned");
 		    }
-		    LOGGER.fatal("Give area of the house: ");
+		    LOGGER.fatal("Give area of the house:\n ");
 		    double area=sc.nextDouble();
 		    if(material==0)
 		    {
 		    	constructionCost=area*1200;
-		    	LOGGER.fatal("The Construction cost in INR:"+constructionCost);
+		    	LOGGER.fatal("The Construction cost in INR:\n"+constructionCost);
 		    }
 		    else  if(material==1)
 		    {
 		    	constructionCost=area*1500;
-		    	LOGGER.fatal("The Construction cost in INR:"+constructionCost);
+		    	LOGGER.fatal("The Construction cost in INR:\n"+constructionCost);
 		    }
 		    else  if(material==2)
 		    {
 		    	constructionCost=area*1800;
-		    	LOGGER.fatal("The Construction cost in INR:"+constructionCost);
+		    	LOGGER.fatal("The Construction cost in INR:\n"+constructionCost);
 		    }
 		    else if(material==3)
 		    {
 		    	constructionCost=area*2500;
-		    	LOGGER.fatal("The Construction cost in INR:"+constructionCost);
+		    	LOGGER.fatal("The Construction cost in INR:\n"+constructionCost);
 		    }
 		    else {
-		    	LOGGER.fatal("Cannot Calculate the Constrution Cost");
+		    	LOGGER.fatal("Cannot Calculate the Constrution Cost\n");
 		    }
 		    	
 		   
